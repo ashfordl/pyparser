08e57b653338d33c232dbfcc088436b3f3d1cf59
ashfordl <luke.a.ashford@gmail.com>
2014-09-18 15:36:27 +0100

    Remove current_type variable

--- src/pyparser.py
+++ src/pyparser.py
@@ -13,7 +13,6 @@ def find_closing_bracket(string, opening_index):
 def convert_to_list(expression):
     exp = []
 
-    current_type = ""
     current_term = ""
 
     i = 0
@@ -21,31 +20,29 @@ def convert_to_list(expression):
         char = expression[i]
 
         # If the character is a digit
-        if char.isdigit() and current_type in ["", "integer", "float"]:
+        if char.isdigit():
             current_term += char
-            current_type = current_type if current_type != "" else "integer"
 
         # If the character is a decimal point
-        elif char == "." and current_type in ["", "integer"]:
+        elif char == "." and "." not in current_term:
             try:
                 if expression[i+1].isdigit():
-                    if current_type == "":
-                        current_term += "0"
-                    current_term += "."
-                    current_type = "float"
+                    if current_term == '':
+                        current_term += '0'
+                    current_term += '.'
             except IndexError: # In case x+1 exceeds the string length
                 pass
 
         elif char in OPERATORS: # If the character is an operator
-            if current_term != "":
+            if current_term != '':
                 exp.append(current_term)
-            current_term = ""
-            current_type = ""
+                
+            current_term = ''
             exp.append(char)
 
         # If the character is a bracket
         elif char == "(":
-            if current_type in ["integer", "float"]:
+            if current_term != '':
                 exp.append(current_term)
 
             close = find_closing_bracket(expression, i)
@@ -53,7 +50,6 @@ def convert_to_list(expression):
 
             exp.append(convert_to_list(substring))
 
-            current_type = ""
             current_term = ""
 
             expression = expression[0:i] + expression[close + 1:]
